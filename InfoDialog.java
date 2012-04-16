981a205ed2b49a3845ec5b3e886d7f85a1e09c27
Zimuu <leiyangx2000@hotmail.com>
2012-04-16 23:45:24 +0300

    Update InfoDialog.java

--- InfoDialog.java
+++ InfoDialog.java
@@ -2,6 +2,10 @@ package kth.proj.notepad;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class InfoDialog extends Activity {
 	
@@ -9,5 +13,15 @@ public class InfoDialog extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.infodialog);
+        int i = getIntent().getIntExtra("info", 0);
+        TextView tv = (TextView) findViewById(R.id.info);
+        tv.setText(i);
+        Button close = (Button) findViewById(R.id.close);
+        close.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+        });
     }
 }
