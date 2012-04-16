26cdfb65c7999f0c20eb179de0c9604e2cc52493
Lei Yang <leiyangx2000@hotmail.com>
2012-04-16 18:02:21 +0200

    Initial commit

---
+++ kth/proj/notepad/ConfirmDialog.java
@@ -0,0 +1,13 @@
+package kth.proj.notepad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConfirmDialog extends Activity {
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirmdialog);
+    }
+}
